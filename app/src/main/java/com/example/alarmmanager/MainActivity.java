5b95c64b4e6a8789ead2e91fd3a33bacda0a6ca0
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-26 00:37:56 -0400

    Add AlarmService logic

--- app/src/main/java/com/example/alarmmanager/MainActivity.java
+++ app/src/main/java/com/example/alarmmanager/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.alarmmanager;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final long FREQUENCY_TIME = 5 * 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar calendar = Calendar.getInstance();
+
+        Intent intent = new Intent(this, SimpleService.class);
+        PendingIntent pendingIntent = PendingIntent.getService(this, 0, intent, 0);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), FREQUENCY_TIME, pendingIntent);
     }
 }
